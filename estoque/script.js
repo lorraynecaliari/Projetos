1a301e475b696df4a54b7fa8d785d2660cbfa13e
Lorrayne Victoria Caliari Machado <90619880+lorraynecaliari@users.noreply.github.com>
2022-04-06 14:54:25 -0300

    Update script.js

--- estoque/script.js
+++ estoque/script.js
@@ -1,84 +1,160 @@
-document.getElementById('form').addEventListener('submit', adicionarProduto);
+class Produto{
 
-function adicionarProduto(e){
-	var nome = document.getElementById('nome').value;
-	var qtd = document.getElementById('qtd').value;
-	var valor = document.getElementById('valor').value;
-
-	if(!nome && !qtd && !valor){
-		
-		alert("Preencha todos os campos!");
-		return false;
-	} 
-
-	//array
-	produto = {
-		nome: nome,
-		qtd: qtd,
-		valor: valor
-	};
-
-	//localStorage.setItem('teste','teste1'); - armazenar dados no navegador
-	//console.log(localStorage.getItem('teste')) - retorna o valor
-
-	if(localStorage.getItem('armazena') === null){
-		var produtos = [];
-		produtos.push(produto);
-		localStorage.setItem('armazena', JSON.stringify(produtos));
-	} else {
-		var produtos = JSON.parse(localStorage.getItem('armazena'));
-		produtos.push(produto);
-		localStorage.setItem('armazena', JSON.stringify(produtos));
+	constructor(){
+		this.id = 1;
+		this.arrayProdutos = [];
+		this.editId = null;
 	}
 
-	document.getElementById('form').reset();
+	salvar(){
+		let produto = this.lerDados();
 
-	mostraProduto();
-
-	e.preventDefault();
-}
-
-function removeProduto(nome){
-	if(confirm("Deseja deletar o produto " + nome)){ //pergunta para o usuario
-	var armazena = JSON.parse(localStorage.getItem('armazena'));
-	console.log(armazena);
-
-	 for(var i = 0 ; i < armazena.length; i++){
-		if(armazena[i].nome == nome){
-			armazena.splice(i, 1);
+		if(this.validar(produto)){
+			if(this.editId == null){
+				this.adicionar(produto);
+			}else{
+			this.atualizar(this.editId, produto);
 		}
 	}
 
-	localStorage.setItem('armazena', JSON.stringify(armazena));
+		this.listaTabela();
+		this.cancelar();
+	}
 
-	mostraProduto();
+	listaTabela(){
+		let saida = document.getElementById('saida');
+		saida.innerText = '';
+
+		for(let i = 0; i < this.arrayProdutos.length; i++){
+			//adicionar linha
+			let tr = saida.insertRow();
+
+			//adicionar coluna
+			let td_id= tr.insertCell();
+			let td_produto= tr.insertCell();
+			let td_qtd= tr.insertCell();
+			let td_valor= tr.insertCell();
+			let td_acao= tr.insertCell();
+
+			//adiconar dados na coluna
+			td_id.innerText = this.arrayProdutos[i].id;
+			td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+			td_qtd.innerText = this.arrayProdutos[i].qtd;
+			td_valor.innerText = this.arrayProdutos[i].valor;
+
+			//botão editar
+			let btnEdit = document.createElement('button');
+			btnEdit.innerHTML = "Editar";
+            btnEdit.setAttribute("onclick", "produto.editar("+ JSON.stringify(this.arrayProdutos[i]) +")");
+
+			//botão deletar
+			let btnDelete = document.createElement('button');
+			btnDelete.innerHTML = "Deletar";
+			btnDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")");
+
+			td_acao.appendChild(btnEdit);
+            td_acao.appendChild(btnDelete);
+            
+            //estilizar o btn
+            btnEdit.style.backgroundColor = "#008CBA";
+            btnEdit.style.color = "white";
+            btnEdit.style.border = "none";
+            btnEdit.style.padding = "12px 16px";
+            btnEdit.style.cursor = "pointer";
+            btnEdit.style.marginRight = "4px";
+
+            btnDelete.style.backgroundColor = "#f44336";
+            btnDelete.style.color = "white";
+            btnDelete.style.border = "none";
+            btnDelete.style.padding = "12px 16px";
+            btnDelete.style.cursor = "pointer";
+
+		}
+	}
+
+	adicionar(produto){
+		produto.qtd = parseFloat(produto.qtd);
+		produto.valor = parseFloat(produto.valor);
+		//adicionar elementos no array
+		this.arrayProdutos.push(produto);
+		this.id++;
+	}
+
+	atualizar(id, produto){
+		for(let i = 0; i < this.arrayProdutos.length; i++){
+			if(this.arrayProdutos[i].id == id){
+				this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+				this.arrayProdutos[i].qtd = produto.qtd;
+				this.arrayProdutos[i].valor = produto.valor;
+			}
+		}
+	}
+
+	editar(dados){
+		this.editId = dados.id;
+
+		document.getElementById('produto').value = dados.nomeProduto;	
+		document.getElementById('qtd').value = dados.qtd;
+		document.getElementById('valor').value = dados.valor;
+
+		//mudar botao para atualizar
+		document.getElementById('btn').innerText = 'Atualizar';
+	}
+
+	lerDados(){
+		let produto ={}
+
+		produto.id = this.id;
+		produto.nomeProduto = document.getElementById('produto').value;
+		produto.qtd = document.getElementById('qtd').value;
+		produto.valor = document.getElementById('valor').value;
+
+		return produto;
+	}
+
+	validar(produto){
+		let msg = '';
+
+		if(produto.nomeProduto == ''){
+			msg += 'Informe o nome do produto \n';
+		}
+
+		if(produto.valor == ''){
+			msg += 'Informe o valor do produto \n';
+		}
+
+		if(produto.qtd == ''){
+			msg += 'Informe a quantidade do produto \n';
+		}
+
+		if(msg != ''){
+			alert(msg);
+			return false
+		}
+		return true;
+	}
+
+	cancelar(){
+		document.getElementById('produto').value = '';
+		document.getElementById('qtd').value = '';
+		document.getElementById('valor').value = '';
+
+		document.getElementById('btn').innerText = 'Salvar';
+		this.editId = null;
+	}
+
+	deletar(id){
+		if(confirm('Deseja deletar o produto ' + id)){
+		let saida = document.getElementById('saida');
+
+		for(let i = 0; i < this.arrayProdutos.length; i++){
+			if(this.arrayProdutos[i].id == id){
+				this.arrayProdutos.splice(i,1);
+				saida.deleteRow(i);
+			}
+		}
 	}
 }
-
-function editaProduto(qtd){
-
 }
 
-function pesquisarProduto(nome){
-
-}
-
-function mostraProduto(){
-	var produtos = JSON.parse(localStorage.getItem('armazena'));
-	var armResultado = document.getElementById('resultado');
-
-	armResultado.innerHTML = ''; //mostrar na tabela
-
-	for(var i = 0; i < produtos.length; i++){
-		var nome = produtos[i].nome;
-		var qtd = produtos[i].qtd;
-		var valor = produtos[i].valor;
-
-		armResultado.innerHTML += '<tr><td>'+ nome + '</td>'+
-		 			'<td>'+ qtd + '</td>' +
-		 			'<td>'+ valor + '</td>' +
-		 			'<td><button onclick="removeProduto(\''+ nome +'\')" class="btnRem">Remover</button>'+ 
-					'<button onclick="editaProduto(\''+ qtd +'\')" class="btnEdt">Atualizar</button></td>'+
-		 			'</tr>';
-	}
-}
+var produto = new Produto();
